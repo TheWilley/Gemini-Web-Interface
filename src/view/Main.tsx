75cd2ff4b0102554a4fa9bed2c76828698356593
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-03-02 18:14:22 +0100

    fix: model is no longer shown in options view

--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -29,13 +29,15 @@ function Main({
   return (
     <div className='relative flex w-full items-center justify-center'>
       <div className='absolute left-2 top-2'>
-        <Dropdown
-          options={models}
-          onSelect={(selected) => {
-            updateSelectModel(selected);
-          }}
-          value={selectedModel.key}
-        />
+        {!viewOptions && (
+          <Dropdown
+            options={models}
+            onSelect={(selected) => {
+              updateSelectModel(selected);
+            }}
+            value={selectedModel.key}
+          />
+        )}
       </div>
       <div className='flex h-screen w-full max-w-[840px] flex-col pl-3 pr-3 pt-12'>
         <div className='no-scrollbar flex-grow overflow-auto p-5'>
