61bac93e656929f82949f8bad98199dee3213293
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-03-02 14:25:25 +0100

    feat: tooltip now supports offsets

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -6,27 +6,61 @@ function Tooltip({
   position,
   children,
   classes,
+  offsetX = 0,
+  offsetY = 0,
 }: {
   text: string;
   position: 'left' | 'right' | 'bottom' | 'top';
   children: ReactNode;
   classes?: string;
+  offsetX?: number;
+  offsetY?: number;
 }) {
-  const tooltipPositionClasses = {
-    top: 'bottom-full mb-2 left-1/2 transform -translate-x-1/2',
-    right: 'left-full ml-2 top-1/2 transform -translate-y-1/2',
-    bottom: 'top-full mt-2 left-1/2 transform -translate-x-1/2',
-    left: 'right-full mr-2 top-1/2 transform -translate-y-1/2',
+  const tooltipPositionStyles = {
+    top: {
+      bottom: '100%',
+      left: '50%',
+      marginBottom: '2px',
+      transform: `translateX(calc(-50% + ${offsetX}px)) translateY(${offsetY}px)`,
+    },
+    right: {
+      left: '100%',
+      top: '50%',
+      marginLeft: '2px',
+      transform: `translateX(${offsetX}px) translateY(calc(-50% + ${offsetY}px))`,
+    },
+    bottom: {
+      top: '100%',
+      left: '50%',
+      marginTop: '2px',
+      transform: `translateX(calc(-50% + ${offsetX}px)) translateY(${offsetY}px)`,
+    },
+    left: {
+      right: '100%',
+      top: '50%',
+      marginRight: '2px',
+      transform: `translateX(${offsetX}px) translateY(calc(-50% + ${offsetY}px))`,
+    },
   };
+
+  for (const positionKey in tooltipPositionStyles) {
+    if (positionKey === 'top' || positionKey === 'bottom') {
+      tooltipPositionStyles[positionKey as keyof typeof tooltipPositionStyles].transform =
+        `translateX(calc(-50% + ${offsetX}px)) translateY(${offsetY}px)`;
+    } else {
+      tooltipPositionStyles[positionKey as keyof typeof tooltipPositionStyles].transform =
+        `translateX(${offsetX}px) translateY(calc(-50% + ${offsetY}px))`;
+    }
+  }
 
   return (
     <span className={classNames('group/tooltip relative inline-block', classes)}>
       {children}
       <span
         className={classNames(
-          'absolute z-10 w-fit scale-75 whitespace-nowrap rounded-md bg-gray-300 px-2 py-1 text-xs text-gray-900 opacity-0 transition-all group-hover/tooltip:scale-100 group-hover/tooltip:opacity-100',
-          tooltipPositionClasses[position]
+          'pointer-events-none absolute z-10 w-fit scale-75 whitespace-nowrap rounded-md bg-gray-300 px-2 py-1 text-xs text-gray-900 opacity-0 transition-all group-hover/tooltip:scale-100 group-hover/tooltip:opacity-100'
         )}
+        style={tooltipPositionStyles[position]}
       >
         {text}
       </span>
