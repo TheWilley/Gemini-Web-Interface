1adb15514f4ad8e8b5639412059db23100ecff7f
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-03-05 22:12:14 +0100

    refactor: restructured input component for easier parsing

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,19 @@
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 import { ChangeEvent } from 'react';
 
+interface InputProps {
+  label: string;
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type: 'text' | 'number';
+  icon: FontAwesomeIconProps['icon'];
+  disabled: boolean;
+  step?: number;
+  minValue?: number;
+  maxValue?: number;
+}
+
 function Input({
   label,
   name,
@@ -12,72 +25,58 @@ function Input({
   step,
   minValue,
   maxValue,
-}: {
-  label: string;
-  name: string;
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  type: 'text' | 'number';
-  icon: FontAwesomeIconProps['icon'];
-  disabled: boolean;
-  step?: number;
-  minValue?: number;
-  maxValue?: number;
-}) {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.validity.valid) return;
-
+}: InputProps) {
+  const handleNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
     let newValue = e.target.value;
 
-    if (type === 'number') {
-      if (minValue !== undefined && +newValue < minValue) {
-        newValue = minValue.toString();
-      } else if (maxValue !== undefined && +newValue > maxValue) {
-        newValue = maxValue.toString();
-      }
+    if (minValue !== undefined && +newValue < minValue) {
+      newValue = minValue.toString();
+    } else if (maxValue !== undefined && +newValue > maxValue) {
+      newValue = maxValue.toString();
     }
 
-    const syntheticEvent = {
-      ...e,
-      target: {
-        ...e.target,
-        value: newValue,
-      },
-    };
+    onChange({ ...e, target: { ...e.target, value: newValue } });
+  };
 
-    onChange(syntheticEvent);
+  const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.type === 'number') {
+      handleNumberChange(e);
+    } else {
+      handleTextChange(e);
+    }
   };
 
   return (
-    <>
-      <div className='flex w-full flex-row-reverse items-stretch'>
-        <input
-          id={name}
-          name={name}
-          value={value}
-          onChange={handleChange}
-          type={type}
-          placeholder={label}
-          aria-label={label}
-          className={`peer block w-full appearance-none rounded rounded-bl-none rounded-tl-none border border-border bg-overlay px-3 py-3 transition-colors duration-300 focus:border-primary focus:outline-none focus:ring-0 ${
-            disabled ? 'bg-gray-200' : ''
-          }`}
-          disabled={disabled}
-          min={type === 'number' && minValue !== undefined ? minValue : undefined}
-          max={type === 'number' && maxValue !== undefined ? maxValue : undefined}
-          step={type === 'number' && step !== undefined ? step : undefined}
-          pattern={type === 'number' ? '[0-9]*' : '*'}
-        />
+    <div className='flex w-full flex-row-reverse items-stretch'>
+      <input
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        type={type}
+        placeholder={label}
+        aria-label={label}
+        className={`peer block w-full appearance-none rounded rounded-bl-none rounded-tl-none border border-border bg-overlay px-3 py-3 transition-colors duration-300 focus:border-primary focus:outline-none focus:ring-0 ${
+          disabled ? 'bg-gray-200' : ''
+        }`}
+        disabled={disabled}
+        min={type === 'number' && minValue !== undefined ? minValue : undefined}
+        max={type === 'number' && maxValue !== undefined ? maxValue : undefined}
+        step={type === 'number' && step !== undefined ? step : undefined}
+      />
 
-        <div
-          className={`flex items-center rounded rounded-br-none rounded-tr-none border border-r-0 border-border px-3 py-3 ${
-            disabled ? 'bg-gray-200' : ''
-          }`}
-        >
-          {<FontAwesomeIcon icon={icon} />}
-        </div>
+      <div
+        className={`flex items-center rounded rounded-br-none rounded-tr-none border border-r-0 border-border px-3 py-3 ${
+          disabled ? 'bg-gray-200' : ''
+        }`}
+      >
+        <FontAwesomeIcon icon={icon} />
       </div>
-    </>
+    </div>
   );
 }
 
